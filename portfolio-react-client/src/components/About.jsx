56de03b02ed836b45226258e44f8a1774aae7198
Jordan Tab <taboglut@bu.edu>
2023-03-07 01:21:10 -0500

    Made the about me page responsive on laptop and monitor

--- portfolio-react-client/src/components/About.jsx
+++ portfolio-react-client/src/components/About.jsx
@@ -8,20 +8,20 @@ const About = () => {
             </div>
             <hr className='horizontal-line'/>
             <div className="about-description">
-                I'm an undergraduate senior student-athlete at Boston University, pursuing a dual-degree in computer science and 
-                business administration with a concentration in business analytics. I'm also a member of the Boston University
-                NCAA Division 1 Varsity Men's Basketball team. Currently, I'm looking for full-time software engineering positions
-                for post-gradution in May 2023.
+                I'm an undergraduate <b>senior student-athlete</b> at <b>Boston University</b>, pursuing a dual-degree in <b>computer 
+                science and business administration</b> with a concentration in <b>business analytics</b>. I'm also a member of the 
+                Boston University NCAA Division 1 <b>Varsity Men's Basketball</b> team. Currently, I'm looking for <b>full-time software engineering 
+                positions</b> for post-gradution in May 2023.
                 <br></br>
                 <br></br>
-                Through my studies, I have gained a solid understanding of full-stack development, and have had the opportunity to work
-                on various projects involving web application design and machine learning. Along with my technical skills, I have also 
+                Through my studies, I have gained a solid understanding of <b>full-stack development</b>, and have had the opportunity to work
+                on various projects involving <b>web application design</b> and <b>machine learning</b>. Along with my technical skills, I havse also 
                 developed project leadership skills through managing and guiding teams towards successful project completion. As a driven
                 and ambitious individual, I am eager to continue expanding on my knowledge and honing my skills in software engineering while
                 contributing to impactful projects.
                 <br></br>
                 <br></br>
-                Besides softare engineering, I enjoy playing basketball, watching sports, reading, studying French, and traveling.
+                Besides softare engineering, I enjoy <b>playing basketball</b>, <b>watching sports</b>, <b>reading</b>, <b>studying French</b>, and <b>traveling</b>.
                 <br></br>
                 <br></br>
                 Here are some technologies I have worked with recently:
@@ -41,6 +41,7 @@ const About = () => {
             <div className="about-education">
                 EDUCATION
                 <h2>Boston University</h2>
+                <h3>2019 - 2023</h3>
                 <p>
                     BA in Computer Science
                     <br></br>
@@ -51,16 +52,16 @@ const About = () => {
                     Dean's List (2020,2021,2022)
                 </p>
             </div>
-            <div className="about-experience">
+            <div className="about-experience-1">
                 EXPERIENCE
                 <h2>Qualcomm Inc.</h2>
-                <p>
-                    Software Engineering Intern
-                </p>
-                {/* <h2>Invite Education LLC.</h2>
-                <p>
-                    Summer Intern
-                </p> */}
+                <h3>Summer 2022</h3>
+                <p>Software Engineering Intern</p>
+            </div>
+            <div className="about-experience-2">
+                <h2>Invite Education LLC.</h2>
+                <h3>Summer 2021</h3>
+                <p>Summer Intern</p>
             </div>
             <br></br>
             <div className="about-bottom" id="about">hey</div>
